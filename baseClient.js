928539abf5ef539f4bbdd4edc7c35f4e7d10c39a
Katie Hull <katie.ann.hull@gmail.com>
2020-08-08 19:06:46 -0700

    Adding commentClient and postClient

--- baseClient.js
+++ baseClient.js
@@ -1,12 +1,17 @@
+/**
+ * Base client for making http requests to server
+ * 
+ */
+
 const fetch = require('node-fetch');
 
 //Speficy base URL for this endpoint.  Params and query string are constructed in classes
-const baseUrl = 'https://jsonplaceholder.typicode.com/';
+const baseUrl = 'https://jsonplaceholder.typicode.com';
 
 //Generic function to make requests of specifiable method, url string, headers, and body
-function makeRequest(urlStr, method, headers={}, body=null) {
+function makeRequest(path, method, headers={}, body=null) {
     let response = {};
-    fetch(baseUrl + urlStr, {
+    fetch(baseUrl + path, {
         method: method,
         headers: headers,
         body: (body ? JSON.stringify(body) : null)
@@ -23,24 +28,24 @@ function makeRequest(urlStr, method, headers={}, body=null) {
 }
 
 //Performs a get request to JSONPlaceholder API
-function get(url, headers={}, body=null) {
-    let response = makeRequest(url, 'GET', headers, body);
+function get(path, headers={}, body=null) {
+    let response = makeRequest(path, 'GET', headers, body);
     console.log(response);
     return response;
 }
 
-function post(url, headers={}, body=null) {
-    let response = makeRequest(url, 'POST', headers, body);
+function post(path, headers={}, body=null) {
+    let response = makeRequest(path, 'POST', headers, body);
     return response;
 }
 
-function del() {
-    let response = makeRequest(url, 'DELETE', headers, body);
+function del(path, headers={}, body=null) {
+    let response = makeRequest(path, 'DELETE', headers, body);
     return response;
 }
 
-function put() {
-    let response = makeRequest(url, 'PUT', headers, body);
+function put(path, headers={}, body=null) {
+    let response = makeRequest(path, 'PUT', headers, body);
     return response;
 }
 
