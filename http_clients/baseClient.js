f3d8b00022d761710756461cfc26258ef2b83576
Katie Hull <katie.ann.hull@gmail.com>
2020-08-10 22:43:15 -0700

    Adding functional tests for getting posts; adding Chai for assertions'

---
+++ http_clients/baseClient.js
@@ -0,0 +1,76 @@
+/**
+ * Base client for making http requests to server
+ * 
+ */
+
+const fetch = require('node-fetch');
+
+//Speficy base URL for this endpoint.  Params and query string are constructed in classes
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+//Generic function to make requests of specifiable method, url string, headers, and body
+function makeRequest(path, method, headers={}, body=null) {
+    return new Promise((resolve, reject) => {
+        var response = {status: null, body: null};
+        fetch(baseUrl + path, {
+            method: method,
+            headers: headers,
+            body: (body ? JSON.stringify(body) : null)
+        })
+        .then(res => {
+            response.status = res.status;
+            res.json().then(resBody => {
+                response.body = resBody;
+                resolve(response);
+            });
+        })
+        .catch(error => {return reject(error)});
+    });
+}
+
+//Performs a get request to JSONPlaceholder API
+function get(path, headers={}, body=null) {
+    return new Promise ((resolve, reject) => {
+        makeRequest(path, 'GET', headers, body)
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {return reject(error)});
+    })
+}
+
+function post(path, headers={}, body=null) {
+    return new Promise((resolve, reject) => {
+        makeRequest(path, 'POST', headers, body)
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {return reject(error)});
+    })
+}
+
+function del(path, headers={}, body=null) {
+    return new Promise((resolve, reject) => {
+        makeRequest(path, 'DELETE', headers, body)
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {return reject(error)});
+    })
+}
+
+function put(path, headers={}, body=null) {
+    return new Promise((resolve, reject) => {
+        makeRequest(path, 'PUT', headers, body)
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {return reject(error)});
+    })
+}
+
+
+exports.get = get;
+exports.post = post;
+exports.del = del;
+exports.put = put;
